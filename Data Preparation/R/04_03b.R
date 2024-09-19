84894ded60eb41926929e7ca4d7ecb93591b9ef0
ibtissam09 <ibtissam.makdoun@gmail.com>
2024-09-19 14:48:48 +0200

    Sampling data using R

---
+++ Data Preparation/R/04_03b.R
@@ -0,0 +1,22 @@
+# Load necessary libraries
+library(readr)
+library(dplyr)
+library(caret)
+
+# Load the dataset
+df <- read_csv('../data/customer_loan_data.csv')
+
+# Display the first few rows of the data
+
+
+# Set the response variable
+
+
+# Define the predictors
+
+
+# Train-test split
+
+
+# Check the distribution of the 'purpose' column in the test set
+
--- Data Preparation/R/04_03e.R
+++ Data Preparation/R/04_03e.R
@@ -1,2 +1,31 @@
-# Handling outliers
+# Load necessary libraries
+library(readr)
+library(dplyr)
+library(caret)
 
+# Load the dataset
+df <- read_csv('../data/customer_loan_data.csv')
+
+# Display the first few rows of the data
+head(df)
+
+# Set the response variable
+response <- 'decision'
+y <- df %>% select(all_of(response))
+head(y)
+
+# Define the predictors
+predictors <- setdiff(names(df), response)
+X <- df %>% select(all_of(predictors))
+head(X)
+
+# Train-test split
+set.seed(1234)
+trainIndex <- createDataPartition(df$purpose, p = 0.99, list = FALSE)
+X_train <- X[trainIndex, ]
+X_test <- X[-trainIndex, ]
+y_train <- y[trainIndex, ]
+y_test <- y[-trainIndex, ]
+
+# Check the distribution of the 'purpose' column in the test set
+prop.table(table(X_test$purpose))
